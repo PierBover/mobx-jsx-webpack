eeb73c4e8be750be6ef9cc1a9a9f7fa7f5cd240f
Pier Bover <pierbover11@gmail.com>
2019-06-13 08:51:57 -0500

    add action

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import {observable} from 'mobx';
+import {observable, action} from 'mobx';
 
 export default function App () {
 	const state = observable({
 		randomNumber: 0
 	});
 
-	setInterval(() => {
+	const updateNumber = action(() => {
 		state.randomNumber = Math.random();
-		console.log('interval!', state.randomNumber);
+		console.log(state.randomNumber);
+	});
+
+	setInterval(() => {
+		console.log('interval!');
+		updateNumber();
 	}, 1000);
 
 	return (
-		<div>{String(state.randomNumber)}</div>
+		<div>{state.randomNumber}</div>
 	);
 }
